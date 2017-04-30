4e72d262c34dc74afb85ee605d366d8ded8583cf
舒达 <shuda.yy@alibaba-inc.com>
2017-04-30 16:56:06 +0800

    增加已结束标的统计

--- content_scriptnew.js
+++ content_scriptnew.js
@@ -6,16 +6,16 @@
     0, 0, 0, 0, 0,
     0, 0, 0, 0, 0,
     0, 0];     // 每日回款额
-  this.income = 0;                                               //每日理论收益
-  this.shareSum = 0;                            //总份数
-  this.valueSum = 0;                                //总价值
-  this.interestDistribution = {};                  //每种利率的份数
+  this.income = 0;     //每日理论收益
+  this.shareSum = 0;   //总份数
+  this.valueSum = 0;   //总价值
+  this.interestDistribution = {};  //每种利率的份数
   this.availableSum = 0;  // 可转让金额
 }
 
 $(function () {
-  var startButton = $("<div style=\"right:0px; height: 25px; width: 216px;position:absolute;background-color: #E1DCEE;z-index: 10000001\">" +
-    "<div style='cursor: pointer;float:left;width:75%' id='calculateButton'>计算收益</div><div style='cursor: pointer;float:right;width:25%' id='displayButton'>显示</div>" +
+  var startButton = $("<div style=\"right:0px; height: 25px; width: 550px;position:absolute;background-color: #E1DCEE;z-index: 10000001\">" +
+    "<div style='cursor: pointer;float:left;width:50%' id='calculateButton'>计算收益</div><div style='cursor: pointer;float:right;width:25%' id='displayButton'>显示</div><div style='cursor: pointer;float:left;width:25%' id='switchToCompletedStatistic'>切换统计已完成标的</div>" +
     "<div id='statisticState'style='display: none;background-color: #E1DCEE;'></div>" +
     "<div id='statisticDiv'style='display: none;background-color: #E1DCEE;' ></div></div>");
 
@@ -28,8 +28,76 @@ $(function () {
   $("#displayButton").click(function () {
     $("#statisticDiv").slideToggle("slow");
   });
+  $('#switchToCompletedStatistic').on('click',completedBidStatistic);
   $("#statisticDiv").html(localStorage.getItem("calculateResult"));
 });
+
+let totalLoanList = [];
+
+const injectData = (loanList) => {
+  loanList.forEach((item)=>{totalLoanList.push(item)})
+}
+
+const renderCompletedBidStatistic = ()=>{
+  totalLoanList.sort(function(a,b){
+    let finishDateA = a.finishDate.slice(0,10);
+    let finishDateB = b.finishDate.slice(0,10);
+    if(parseInt(finishDateA.split('-')[0]) > parseInt(finishDateB.split('-')[0])){
+      return 1;
+    }else if(parseInt(finishDateA.split('-')[0]) < parseInt(finishDateB.split('-')[0])){
+      return -1;
+    }else{
+      if(parseInt(finishDateA.split('-')[1]) > parseInt(finishDateB.split('-')[1])){
+        return 1;
+      }else if(parseInt(finishDateA.split('-')[1]) < parseInt(finishDateB.split('-')[1])){
+        return -1;
+      }else{
+        if(parseInt(finishDateA.split('-')[2]) > parseInt(finishDateB.split('-')[2])){
+          return 1;
+        }else if(parseInt(finishDateA.split('-')[2]) < parseInt(finishDateB.split('-')[2])){
+          return -1;
+        }else{
+          return 0;
+        }
+      }
+    }
+    console.log(finishDateA);
+
+  });
+
+  let tableContent = '';
+  tableContent += '<table style="width: 400px"><tr><th>序号</th><th>借款标ID</th><th>结清时间</th><th>结清方式</th><th>所赚金额</th></tr>'
+  for(let j = 0 ; j < totalLoanList.length; j++){
+    tableContent += '<tr><td>'+(j+1)+'</td><td>'+totalLoanList[j].loanId+'</td><td>'+totalLoanList[j].finishDate+'</td><td>'+totalLoanList[j].finishType+'</td><td>'+totalLoanList[j].earnMoney+'</td></tr>'
+  }
+  tableContent += '</table>'
+  $('body').append(tableContent);
+}
+
+const completedBidStatistic = ()=>{
+  console.log('completedBidStatistic in');
+  $.ajax({
+    url: 'https://www.renrendai.com/account/invest!loanJson.action?loanType=FINISH_LOAN&pageIndex=1'
+  }).done((result)=>{
+    console.log(result.data);
+    const totalPages = result.data.totalPage;
+    const totalCount = result.data.totalCount;
+    injectData(result.data.loanList);
+    for(let i = 2; i <= totalPages; i++){
+      $.ajax({
+        url: 'https://www.renrendai.com/account/invest!loanJson.action?loanType=FINISH_LOAN&pageIndex='+i
+      }).done((result2)=>{
+        injectData(result2.data.loanList);
+        if(i == totalPages){
+          console.log(totalLoanList);
+          setTimeout(()=>{
+            renderCompletedBidStatistic();
+          },1000);
+        }
+      });
+    }
+  });
+}
 
 /*statisticObject 属性
  date[0,1,2,3....] ,     每日回款额
